ec0f09a8a5671db03e706607023ba4e3698f57f4
yacoub33 <yacoubhaddad33@outlook.com>
2023-10-04 14:04:55 +0100

    test

--- src/screens/Product.js
+++ src/screens/Product.js
@@ -6,7 +6,7 @@ function Product({ products }) {
   return (
     <div>
       <Products product={products} />
-      <div className=" absolute md:hidden rounded-md h-[60px] w-full bg-gray-600/70 backdrop-blur-sm bottom-5"></div>
+      <div className=" fixed md:hidden rounded-md h-[60px] w-full bg-gray-600/70 backdrop-blur-sm bottom-5"></div>
       <Recommended />
     </div>
   );
